52683f9ed5e85ba64644cbf8b6aac9a4ec7ee22e
Tsung Hung <hungmasterj@gmail.com>
2015-05-19 19:14:16 -0700

    challenge ten added and also strftime package used

--- program.js
+++ program.js
@@ -76,35 +76,45 @@
 // )})
 
 // #9
-var http = require('http');
-var bl = require('bl');
+// var http = require('http');
+// var bl = require('bl');
+//
+// var results = [],
+//     count = 0;
+//
+// function printResults () {
+//       for (var i = 0; i < 3; i++)
+//         console.log(results[i])
+// }
+//
+// function httpGet (index) {
+//   http.get(process.argv[2 + index], function (response) {
+//     response.pipe(bl(function (err, data) {
+//       if (err) {
+// 				return console.error("Something went wrong: ", err)
+// 			}
+//
+//       results[index] = data.toString()
+//       count += 1;
+//
+//       if (count === 3) {
+// 				printResults();
+// 			}
+//
+//     }))
+//   })
+// }
+//
+// for (var i = 0; i < 3; i++) {
+// 	httpGet(i);
+// }
 
-var results = [],
-    count = 0;
 
-function printResults () {
-      for (var i = 0; i < 3; i++)
-        console.log(results[i])
-}
-
-function httpGet (index) {
-  http.get(process.argv[2 + index], function (response) {
-    response.pipe(bl(function (err, data) {
-      if (err) {
-				return console.error("Something went wrong: ", err)
-			}
-
-      results[index] = data.toString()
-      count += 1;
-
-      if (count === 3) {
-				printResults();
-			}
-
-    }))
-  })
-}
-
-for (var i = 0; i < 3; i++) {
-	httpGet(i);
-}
+// #10
+var net = require('net');
+var server = net.createServer(function(socket) {
+	var strftime = require('strftime');
+	socket.write(strftime('%Y-%m-%d %H:%M', new Date())+'\n')
+	socket.end()
+})
+server.listen(process.argv[2]);
