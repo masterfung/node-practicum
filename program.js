2938adc9d5c2cf70ee65cef45b296c529147a7ed
Tsung Hung <hungmasterj@gmail.com>
2015-05-19 18:58:43 -0700

    juggling async

--- program.js
+++ program.js
@@ -52,10 +52,59 @@
 // });
 
 // #7
-var http = require('http');
+// var http = require('http');
+//
+// http.get(process.argv[2], function(response) {
+// 	response.on("data", function(data) {
+// 		console.log(data.toString());
+// 	})
+// })
 
-http.get(process.argv[2], function(response) {
-	response.on("data", function(data) {
-		console.log(data.toString());
-	})
-})
+
+// #8
+// var http = require('http');
+// var concatStream = require('concat-stream');
+//
+// http.get(process.argv[2], function(response) {
+// 	response.pipe(concatStream(function(err, data) {
+// 		if (err) {
+// 			console.log('Error has occured:', err);
+// 		}
+// 		console.log(data.toString().length);
+// 		console.log(data.toString());
+// 	})
+// )})
+
+// #9
+var http = require('http');
+var bl = require('bl');
+
+var results = [],
+    count = 0;
+
+function printResults () {
+      for (var i = 0; i < 3; i++)
+        console.log(results[i])
+}
+
+function httpGet (index) {
+  http.get(process.argv[2 + index], function (response) {
+    response.pipe(bl(function (err, data) {
+      if (err) {
+				return console.error("Something went wrong: ", err)
+			}
+
+      results[index] = data.toString()
+      count += 1;
+
+      if (count === 3) {
+				printResults();
+			}
+
+    }))
+  })
+}
+
+for (var i = 0; i < 3; i++) {
+	httpGet(i);
+}
