e1f967bc690e574fdb505f19334a7aff16eaaec3
Tsung Hung <hungmasterj@gmail.com>
2015-05-20 20:39:49 -0700

    helping functions with handlebars

---
+++ hapi/helping.js
@@ -0,0 +1,27 @@
+var Hapi = require('hapi');
+var Path = require('path');
+
+var server = new Hapi.Server();
+
+server.connection({
+  host: 'localhost',
+  port: Number(process.argv[2] || 8080)
+});
+
+server.views({
+  engines: {
+    html: require('handlebars')
+  },
+  path: Path.join(__dirname, 'templates'),
+  helpersPath: Path.join(__dirname, 'helpers')
+});
+
+server.route({
+  path: '/',
+  method: 'GET',
+  handler: {
+    view: "template.html"
+  }
+})
+
+server.start();
